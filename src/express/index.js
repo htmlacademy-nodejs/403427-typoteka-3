b00965623b04541eae4ed8df1a62587c412dd9e0
Roman Belik <rombelik@yandex.ru>
2021-01-20 20:37:34 +0300

    added templates

--- src/express/index.js
+++ src/express/index.js
@@ -5,8 +5,10 @@ const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const categoriesRoutes = require(`./routes/categories-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
@@ -14,5 +16,17 @@ app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/categories`, categoriesRoutes);
 app.use(`/`, mainRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res, next) => {
+  res.status(500).render(`errors/500`);
+  next();
+});
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
